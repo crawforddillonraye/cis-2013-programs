04471f914469f9136f4285ce88867afc0911d2c5
crawforddillonraye <54999261+crawforddillonraye@users.noreply.github.com>
2019-10-24 23:27:37 -0500

    Add files via upload

---
+++ dom2013grade.js
@@ -0,0 +1,71 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+};
+
+var calculate_click = function () 
+{
+    var floatHwPts, floatMidPts, floatFinPts, stringFinalGrade, floatTotalPts, intGradeOption;
+
+    floatHwPts = parseFloat($("hw_pts").value);
+    floatMidPts = parseFloat($("mid_pts").value);
+    floatFinPts = parseFloat($("fin_pts").value);
+    floatTotalPts = (floatHwPts + floatMidPts + floatFinPts);
+    intGradeOption = parseInt($("grade_option").value);
+    stringFinalGrade =($("final_grade").value);
+
+
+if (intGradeOption==1)
+{
+    if (floatTotalPts >=80)
+        {
+            stringFinalGrade =("pass");
+        }
+     else if(floatTotalPts <=79)  
+    {
+        stringFinalGrade =("fail");
+    }
+}
+
+    if (intGradeOption==2)
+    {
+      
+      if (floatTotalPts >= 90)
+      {
+         stringFinalGrade =("A");
+      }
+      
+   if(floatTotalPts <= 89 && floatTotalPts >= 80) 
+      {
+        stringFinalGrade =("B");
+      }
+    if (floatTotalPts >= 70  && floatTotalPts <= 79) 
+      {
+         stringFinalGrade =("C");
+      }  
+         if (floatTotalPts <= 69 && floatTotalPts >= 60) 
+      {
+         stringFinalGrade =("D");
+      }
+      
+         if (floatTotalPts <= 59 && floatTotalPts>=0)
+         {
+            stringFinalGrade =("F");
+         }
+        
+      }
+   $("final_grade").value =  stringFinalGrade;
+}
+
+
+
+
+
+
+window.onload = function () 
+{
+    $("final_grade").value = ""; //blanks the final grade text box upon page load
+    $("calculate").onclick = calculate_click; //activates main method when the button is clicked 
+    $("hw_pts").focus(); //puts the cursor on the first DOM text input box
+   
+};
